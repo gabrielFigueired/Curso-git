1fa79d7ac233986b9c2d4c1f0f45bda9613eabe3
Gabriel Figueiredo <bielfigueiredo@gmail.com>
2019-11-06 11:21:03 -0300

    Projeto iniciado

---
+++ Exer_Estrutura_condicional1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Exer_Estrutura_condicional1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número:");
+            int n = int.Parse(Console.ReadLine());
+
+            if (n < 0)
+            {
+                Console.WriteLine("Negativo");
+            }
+            else
+            {
+                Console.WriteLine("Não Negativo");
+            }
+        }
+    }
+}
